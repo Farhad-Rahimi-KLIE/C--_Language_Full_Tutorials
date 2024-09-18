c113f6b2bc4288a2945dc27b501b870bb241915b
Farhad Rahimi Klie <farhadrahimiklie786@gmail.com>
2024-09-18 15:28:43 +0430

    run time polymorphism

---
+++ 24 Polymorphism/Run_Time_Polymorphism.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+ 
+using namespace std;
+
+/*
+ Run Time Polymorphism
+  1 - Function Overriding -> parent and child both contain the same function with different implementation.
+  2 - the parent class function is said to be overriding.
+*/
+// Run time Polymorphism FUnction overriding
+
+// class Parent {
+//     public:
+//      void getInfo(){
+//         cout<<"Parent Class\n";
+//      }
+// };
+
+// class Child : public Parent {
+//     public:
+//      void getInfo(){
+//         cout<<"Child Class\n";
+//      }
+// };
+ 
+int main()
+{
+    // Parent s1;
+    // s1.getInfo();
+    return 0;
+}
